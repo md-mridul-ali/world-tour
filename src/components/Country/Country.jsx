0e8e643eac155fc3dc2226ddb9fa3bdd7afa0a17
md-mridul-ali <mridulaiub17@gmail.com>
2026-03-03 02:56:20 +0600

    conditional rendering

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './Country.css'
 const Country = ({country}) => {
     // console.log(country.name)
+    const [visited, setVisited] = useState(false);
+    const handleVisited = () =>{
+        //    setVisited(true);
+
+        // if(visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+
+        // setVisited(visited ? false : true)
+
+        setVisited(!visited);
+
+    }
+
     return (
-        <div style={{
-            border: '2px solid blue',
-            borderRadius: '20px',
-            marginBottom: '10px',
-            padding: '20px'
-        }}>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Capital: {country.capital.capital}</p>
             <p>Area: {country.area.area}</p>
             <p>Population: {country.population.population}</p>
+            <button onClick={handleVisited}>
+                {
+                    visited ? 'Visted' : 'Not Visited'
+                }
+            </button>
         </div>
     );
 };
